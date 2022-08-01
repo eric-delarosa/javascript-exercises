f074dfd58eac1a0d2b673c1dc4794ee77ad41b28
eric-delarosa <ericd.delarosa@gmail.com>
2022-08-01 16:38:03 -0400

    Complete exercise 05 sumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,22 @@
-const sumAll = function() {
+const sumAll = function(num1, num2) {
+    let sum = 0;
+    let start = num1;
+    let end = num2;
+    if (num1 > num2) {
+        start = num2;
+        end = num1;
+    }
+    if (num1 < 0 || num2 < 0){
+        return 'ERROR';
+    }
+    if (!Number.isInteger(num1) || !Number.isInteger(num2)){
+        return 'ERROR';
+    }
 
+    for (let i = start; i <= end; i++) {
+        sum += i;
+    }
+    return sum;
 };
 
 // Do not edit below this line
