85709576efd5b33aaf3f9ccc3b980a998be2c80b
eric-delarosa <ericd.delarosa@gmail.com>
2022-08-01 15:36:46 -0400

    Add solution to exercise 03

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -6,5 +6,12 @@ const reverseString = function(string) {
     return reversed;
 };
 
+/*
+SOLUTION
+const reverseString = function(string) {
+ return string.split('').reverse().join('');
+};
+*/
+
 // Do not edit below this line
 module.exports = reverseString;
