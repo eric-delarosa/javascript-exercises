3f9fa187416d8d5fb87e3cc22fb90ff46bff14fa
eric-delarosa <ericd.delarosa@gmail.com>
2022-08-01 16:24:46 -0400

    Complete exercise 04 removefromarray

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,18 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array, ...args) {
+    let result = [];
+    for (let i = 0; i < array.length; i++) {
+        let flag = false;
+        for(let arg of args){
+            if (array[i] === arg){
+                flag = true;
+            }
+        }       
+        if (flag !== true){
+            result.push(array[i]);
+        }
+        
+    }
+    return result;
 };
 
 // Do not edit below this line
