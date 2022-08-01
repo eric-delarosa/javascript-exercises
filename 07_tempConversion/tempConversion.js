928df15685933053913e026bf1b9025c5db147db
eric-delarosa <ericd.delarosa@gmail.com>
2022-08-01 17:01:42 -0400

    Complete exercise 07 tempConversion

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,11 @@
-const ftoc = function() {
-
+const ftoc = function(tempF) {
+  // C = (F - 32) * 5/9
+  return Number(((tempF - 32) * (5/9)).toFixed(1));
 };
 
-const ctof = function() {
-
+const ctof = function(tempC) {
+  // F = C * 9/5 + 32
+  return Number((tempC * (9/5) + 32).toFixed(1));
 };
 
 // Do not edit below this line
