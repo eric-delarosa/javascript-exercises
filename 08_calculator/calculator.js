42dbf95a49649b504f24a4886ba18306225693bc
eric-delarosa <ericd.delarosa@gmail.com>
2023-05-05 18:49:18 -0400

    Complete exercise 08-calculator

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,30 +1,41 @@
 const add = function(a, b) {
   return a + b;
-	
+
 };
 
 const subtract = function(a, b) {
-	return a - b;
+  return a - b;
 };
 
 const sum = function(arr) {
-	let total = 0;
-  for(let i = 0; i < arr.length; i++) {
+  let total = 0;
+  for (let i = 0; i < arr.length; i++) {
     total += arr[i];
   }
   return total;
 };
 
-const multiply = function(...args) {
-
+const multiply = function(arr) {
+  let prod = 1;
+  for (let i = 0; i < arr.length; i++) {
+    prod *= arr[i];
+  }
+  return prod;
 };
 
-const power = function() {
-	
+const power = function(num, exp) {
+  let tot = 1;
+  for (let i = 0; i < exp; i++) {
+    tot *= num;
+  }
+  return tot;
 };
 
-const factorial = function() {
-	
+const factorial = function(val) {
+  if (val === 0) {
+    return 1;
+  }
+  return val * factorial(val - 1)
 };
 
 // Do not edit below this line
