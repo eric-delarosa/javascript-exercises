17f267ebaa037a3bb4c74705811f1b91900a41a3
eric-delarosa <ericd.delarosa@gmail.com>
2023-05-05 20:21:40 -0400

    Complete 09-palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,15 @@
-const palindromes = function () {
+const palindromes = function(phrase) {
+  const regEx = /[^A-Za-z0-9]/g;
+  phrase = phrase.toLowerCase().replace(regEx, '');
+  let len = phrase.length;
 
-};
+  for (let i = 0; i < len / 2; i++) {
+    if (phrase[i] !== phrase[len - 1 - i]) {
+      return false;
+    }
+  }
+  return true;
 
+}
 // Do not edit below this line
 module.exports = palindromes;
